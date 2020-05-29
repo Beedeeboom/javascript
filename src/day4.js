ac2dc3f51eda5136dcf0755a9aa55caff16a08c8
Beedeeboom <stephanie.duffieux@gmail.com>
2020-05-29 12:11:46 +1000

    added Closure and Mixins to day 4 lesson

--- src/day4.js
+++ src/day4.js
@@ -71,3 +71,68 @@ function parentFunc(a) {
 
 let answer = parentFunc(10) 
 console.log(answer) //prints 11 => a + b
+
+//Closure
+//A Closure is a function that wraps another function or functions, but rather than calling them itself it returns them
+// A Function == an Object
+function makeAdder(a) {
+    return function(b) { //here we are not calling a function, we are declaring one and returning it at the same time
+        return a + b //here the function will use the parent function and its own argument to return a + b
+    }
+}
+
+const addFive = makeAdder(5) //here we are making a copy of the function makeAdder. Here we are permanently declaring a to be 5
+console.log(addFive(6)) //here we are declaring b to be 6 because we previously declared a to be 5 so we get a result of 11
+//this is referred to as closure.
+//essentially what we are doing here is creating a custom function and returning that custom function.
+//makeAdder is like a builder that creates functions that we customise by passing parameters.
+//here we are asking makeAdder to make something with 5, so add something to 5
+
+//Mixins
+//We can abstract out functionality and use the mixin as a way to do multiple inheritance
+
+class Shape {
+    constructor(height, width) {
+        this.height = height
+        this.width = width
+    }
+    talk(thing) {
+        return `Hey! I am a talking ${thing}!`
+    }
+}
+
+class Square extends Shape {
+    constructor(width) {
+        super(width, width)
+    }
+    
+    talk() {
+        return super.talk("Square") //here we are using the parent class "Shape" and passing "Square" to talk()
+     }
+}
+
+//Anonymous Class Mixin
+//This allows us to re-use a class in a different hierarchy
+//Mixins allow us to re-use a certain function 
+const calcMixin = function(Base) { //here we create an anonymous class that inherites from any class we want
+    return class extends Base { 
+        area() {
+            return this.width * this.height 
+        }
+    }
+}
+
+class Square extends calcMixin(Shape) {
+    constructor(width) {
+        super(width, width)
+    }
+    
+    talk() {
+        return super.talk("Square") 
+     }
+}
+
+let sq = new Square(10, 10)
+
+console.log(sq.talk())
+console.log(sq.area())
